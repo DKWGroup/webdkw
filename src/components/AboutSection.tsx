9b55d0c21282ae03e8bdb44337f7efaf2d8c3deb
DKWGroup <contact.dkwgroup@gmail.com>
2025-06-30 15:05:10 +0200

    Fix TypeScript unused import errors

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Award, Users, TrendingUp, Coffee, ArrowRight } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Code, Mail, Phone, ExternalLink } from 'lucide-react'
+import { Mail, Phone, ExternalLink } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
