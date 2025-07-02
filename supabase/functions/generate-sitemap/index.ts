ee7d62f74d72b69d72cd8b5df0fa4d19182ec1f0
DKWGroup <contact.dkwgroup@gmail.com>
2025-07-02 17:18:10 +0200

    Fix sitemap MIME type error

--- supabase/functions/generate-sitemap/index.ts
+++ supabase/functions/generate-sitemap/index.ts
@@ -137,7 +137,7 @@ serve(async (req) => {
 
     // 5. Kompresuj, jeśli wymagane
     let responseBody: Uint8Array | string = xml
-    let contentType = 'application/xml'
+    let contentType = 'text/xml'
     let contentEncoding = ''
 
     if (compress) {
