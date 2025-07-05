963da7bdf58709530fe0fce14a3efe6b6aa41c89
DKWGroup <contact.dkwgroup@gmail.com>
2025-07-05 13:41:58 +0200

    Updated SilesianLandingPage.tsx

--- src/pages/SilesianLandingPage.tsx
+++ src/pages/SilesianLandingPage.tsx
@@ -202,8 +202,8 @@ const SilesianLandingPage = () => {
         <section className="relative bg-gradient-to-br from-yellow-700 via-yellow-800 to-yellow-900 text-white py-20 overflow-hidden">
           <div className="absolute inset-0 bg-black/20"></div>
           <div className="absolute inset-0">
-            <div className="absolute top-20 left-10 w-72 h-72 bg-yellow-500/10 rounded-full blur-3xl"></div>
-            <div className="absolute bottom-20 right-10 w-96 h-96 bg-yellow-500/10 rounded-full blur-3xl"></div>
+            <div className="absolute top-20 left-10 w-72 h-72 bg-blue-500/10 rounded-full blur-3xl"></div>
+            <div className="absolute bottom-20 right-10 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl"></div>
           </div>
           
           <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
