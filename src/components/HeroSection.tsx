c34df9c1dfc74e5f69dda0407d0995dc4638847e
DKWGroup <contact.dkwgroup@gmail.com>
2025-07-28 15:58:53 +0200

    Fix missing Link import in HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { ArrowRight, ChevronLeft, ChevronRight } from 'lucide-react'
 
 const HeroSection = () => {
